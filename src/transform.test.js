72e4e02db1ce015da8c230d38778e5e462847a79
doubleswirve <saxon.stiller@gmail.com>
2018-03-12 16:40:20 -0400

    yet another passing test

--- src/transform.test.js
+++ src/transform.test.js
@@ -1,6 +1,6 @@
 import transform from './transform'
 
-test('QB data -> ES query', () => {
+test('(0) QB data -> ES query', () => {
   expect(transform({
     condition: 'AND',
     rules: [
@@ -17,6 +17,81 @@ test('QB data -> ES query', () => {
     bool: {
       must: [
         { match: { name: '123' } }
+      ]
+    }
+  })
+})
+
+test('(1) QB data -> ES query', () => {
+  expect(transform({
+    condition: 'OR',
+    rules: [
+      {
+        id: 'name',
+        field: 'name',
+        type: 'string',
+        input: 'text',
+        operator: 'contains',
+        value: '123'
+      }
+    ]
+  })).toEqual({
+    bool: {
+      should: [
+        { match: { name: '123' } }
+      ]
+    }
+  })
+})
+
+test('(1) QB data -> ES query', () => {
+  expect(transform({
+    condition: 'AND',
+    rules: [
+      {
+        id: 'name',
+        field: 'name',
+        type: 'string',
+        input: 'text',
+        operator: 'contains',
+        value: '123'
+      },
+      {
+        condition: 'or',
+        rules: [
+          {
+            id: 'misc',
+            field: 'misc',
+            type: 'string',
+            input: 'text',
+            operator: 'equal',
+            value: '123'
+          },
+          {
+            id: 'type',
+            field: 'type',
+            type: 'string',
+            input: 'checkbox',
+            operator: 'in',
+            value: [
+              'book'
+            ]
+          }
+        ]
+      }
+    ]
+  })).toEqual({
+    bool: {
+      must: [
+        { match: { name: '123' } },
+        {
+          bool: {
+            should: [
+              { match_phrase: { misc: '123' } },
+              { terms: { type: ['book'] } }
+            ]
+          }
+        }
       ]
     }
   })
