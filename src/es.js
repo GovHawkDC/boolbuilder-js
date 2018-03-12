0073c2588928ed49c280e30bb66f38bb3cab21ee
doubleswirve <saxon.stiller@gmail.com>
2018-03-12 16:04:43 -0400

    src file for main fragment transforms + tests

---
+++ src/es.js
@@ -0,0 +1,121 @@
+function getArrayValue (value) {
+  if (Array.isArray(value)) {
+    return value
+  }
+
+  if (typeof value === 'string') {
+    return value.split(',').map(v => v.trim())
+  }
+
+  throw new Error(
+    `Unable to build ES bool query with value type: "${typeof value}"`
+  )
+}
+
+function getClause (group, rule) {
+  const { condition = 'AND' } = group
+  const { operator } = rule
+
+  switch (condition.toUpperCase()) {
+    case 'OR':
+      return 'should'
+    case 'AND':
+      return isNegativeOperator(operator) ? 'must_not' : 'must'
+    default:
+      throw new Error(
+        `Unable to build ES bool query with condition: "${condition}"`
+      )
+  }
+}
+
+function getFragment (rule) {
+  const { field, operator } = rule
+
+  switch (operator) {
+    case 'is_not_null':
+    case 'is_null':
+      return { exists: { field } }
+    default:
+      return {
+        [getOperator(rule)]: {
+          [field]: getValue(rule)
+        }
+      }
+  }
+}
+
+function getOperator (rule) {
+  const { operator, value } = rule
+
+  if (/.(\*|\?)/.test(value)) {
+    return 'wildcard'
+  }
+
+  switch (operator) {
+    case 'contains':
+      return 'match'
+    case 'equal':
+    case 'not_equal':
+    case 'proximity':
+      return 'match_phrase'
+    case 'in':
+    case 'not_in':
+      return 'terms'
+    default:
+      return 'range'
+  }
+}
+
+function getValue (rule) {
+  const { operator, value } = rule
+
+  switch (operator) {
+    case 'between':
+      const [gte, lte] = value
+      return { gte, lte }
+    case 'contains':
+    case 'equal':
+    case 'is_not_null':
+    case 'is_null':
+    case 'not_equal':
+      return value
+    case 'greater':
+      return { gt: value }
+    case 'greater_or_equal':
+      return { gte: value }
+    case 'in':
+    case 'not_in':
+      return getArrayValue(value)
+    case 'less':
+      return { lt: value }
+    case 'less_or_equal':
+      return { lte: value }
+    case 'proximity':
+      const [query, slop] = value
+      return { query, slop }
+    default:
+      throw new Error(
+        `Unable to build ES bool query with operator: "${operator}"`
+      )
+  }
+}
+
+function isNegativeOperator (operator) {
+  switch (operator) {
+    case 'is_null':
+    case 'not_equal':
+    case 'not_in':
+      return true
+    default:
+      return false
+  }
+}
+
+export {
+  getArrayValue,
+  getClause,
+  getFragment,
+  getOperator,
+  getValue,
+  isNegativeOperator
+}
---
+++ src/es.test.js
@@ -0,0 +1,56 @@
+import {
+  getArrayValue,
+  getClause,
+  getOperator,
+  isNegativeOperator
+} from './es'
+
+// getArrayValue
+test('array arg to getArrayValue returns self', () => {
+  expect(getArrayValue(['1', '2'])).toEqual(['1', '2'])
+})
+
+test('string arg to getArrayValue returns comma-split array', () => {
+  expect(getArrayValue('1, 2')).toEqual(['1', '2'])
+})
+
+test('non-string|array arg to getArrayValue throws', () => {
+  expect(() => getArrayValue(true)).toThrow()
+})
+
+// getClause
+test('default condition + negative to getClause is "must_not"', () => {
+  expect(getClause({}, { operator: 'not_equal' })).toBe('must_not')
+})
+
+test('default condition + non-negative to getClause is "must"', () => {
+  expect(getClause({}, { operator: 'equal' })).toBe('must')
+})
+
+test('OR condition + anything to getClause is "should"', () => {
+  expect(getClause({ condition: 'or' }, { operator: 'not_equal' }))
+    .toBe('should')
+})
+
+test('unhandled condition + anything to getClause throws', () => {
+  expect(() => getClause({ condition: 'xor' }, { operator: 'not_equal' }))
+    .toThrow()
+})
+
+// getOperator
+test('value containing "*" to getOperator is "wildcard"', () => {
+  expect(getOperator({ value: 'hello*' })).toBe('wildcard')
+})
+
+test('value containing "?" to getOperator is "wildcard"', () => {
+  expect(getOperator({ value: 'hello world?' })).toBe('wildcard')
+})
+
+// isNegativeOperator
+test('is_null to isNegativeOperator is true', () => {
+  expect(isNegativeOperator('is_null')).toBe(true)
+})
+
+test('greater to isNegativeOperator is false', () => {
+  expect(isNegativeOperator('greater')).toBe(false)
+})
